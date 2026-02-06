903fbd79a61f693810122d1200d8b5233bacec64
jarzeckil <jarzeckil@gmail.com>
2026-02-06 16:05:22 +0100

    refactor: format files

--- src/skin_disease_recognition/modeling/train.py
+++ src/skin_disease_recognition/modeling/train.py
@@ -1,13 +1,17 @@
 import os.path
 
 import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 
 from skin_disease_recognition.config import PROJECT_ROOT
 from skin_disease_recognition.data.loaders import make_loaders
 
 
-@hydra.main(config_path=os.path.join(PROJECT_ROOT, 'conf'), config_name='config', version_base='1.2')
+@hydra.main(
+    config_path=os.path.join(PROJECT_ROOT, 'conf'),
+    config_name='config',
+    version_base='1.2',
+)
 def train(cfg: DictConfig):
     train_loader, test_loader = make_loaders(cfg)
 
