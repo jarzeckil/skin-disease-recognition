9c83db996c193d65ba33c05b4cd6603a9fa9b6ec
jarzeckil <153275199+jarzeckil@users.noreply.github.com>
2026-02-10 17:32:24 +0100

    feat: add seeding and absolute path

--- src/skin_disease_recognition/data/loaders.py
+++ src/skin_disease_recognition/data/loaders.py
@@ -1,10 +1,20 @@
+import random
+
 import albumentations as A
 from albumentations import ToTensorV2
+import hydra.utils
 import numpy as np
 from omegaconf import DictConfig
+import torch
 from torch.utils.data import DataLoader, WeightedRandomSampler
 
 from skin_disease_recognition.data.dataset import SkinDataset
+
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
 
 
 def get_transforms(cfg: DictConfig, stage='train'):
@@ -38,14 +48,21 @@ def make_loaders(cfg: DictConfig):
     train_transform = get_transforms(cfg, 'train')
     test_transform = get_transforms(cfg, 'test')
 
-    train_dataset = SkinDataset(cfg.data.train_path, train_transform)
-    test_dataset = SkinDataset(cfg.data.test_path, test_transform)
+    train_dataset = SkinDataset(
+        hydra.utils.to_absolute_path(cfg.data.train_path), train_transform
+    )
+    test_dataset = SkinDataset(
+        hydra.utils.to_absolute_path(cfg.data.test_path), test_transform
+    )
 
     class_weights = 1.0 / np.bincount(train_dataset.targets)
     sample_weights = class_weights[train_dataset.targets]
 
+    g = torch.Generator()
+    g.manual_seed(cfg.seed)
+
     sampler = WeightedRandomSampler(
-        sample_weights.tolist(), num_samples=len(train_dataset)
+        sample_weights.tolist(), num_samples=len(train_dataset), generator=g
     )
 
     train_loader = DataLoader(
@@ -55,12 +72,16 @@ def make_loaders(cfg: DictConfig):
         pin_memory=True,
         sampler=sampler,
         shuffle=False,
+        generator=g,
+        worker_init_fn=seed_worker,
     )
     test_loader = DataLoader(
         dataset=test_dataset,
         batch_size=cfg.data.batch_size,
         num_workers=cfg.data.num_workers,
         pin_memory=True,
+        generator=g,
+        worker_init_fn=seed_worker,
     )
 
     return train_loader, test_loader
