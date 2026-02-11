358f97693eab53a94d76f13ced15080500d8e757
jarzeckil <jarzeckil@gmail.com>
2026-02-11 01:38:23 +0100

    feat: create config for efficientnet

--- src/skin_disease_recognition/modeling/train.py
+++ src/skin_disease_recognition/modeling/train.py
@@ -4,6 +4,7 @@ import os.path
 import hydra
 from omegaconf import DictConfig
 import torch.nn
+import torchvision.models
 
 from skin_disease_recognition.config import PROJECT_ROOT
 from skin_disease_recognition.data.loaders import make_loaders
@@ -26,14 +27,29 @@ def train(cfg: DictConfig):
 
     logger.info('Instantiating objects')
     model: torch.nn.Module = hydra.utils.instantiate(cfg.model.estimator)
+
+    if cfg.model.pretrained:
+        logger.info('Freezing parameters')
+        for param in model.parameters():
+            param.requires_grad = False
+
+    if cfg.model.model_type == 'resnet':
+        model: torchvision.models.ResNet
+        model.fc = torch.nn.Linear(
+            in_features=model.fc.in_features, out_features=cfg.data.num_classes
+        )
+    elif cfg.model.model_type == 'efficientnet':
+        model: torchvision.models.EfficientNet
+        model.classifier[1] = torch.nn.Linear(
+            in_features=model.classifier[1].in_features,
+            out_features=cfg.data.num_classes,
+        )
+
+    model = model.to(device=cfg.device)
+
     loss_fn = hydra.utils.instantiate(cfg.loss_function)
     optim_partial = hydra.utils.instantiate(cfg.optimizer)
     optim = optim_partial(model.parameters())
-
-    if cfg.model.model_type == 'resnet':
-        model.fc = torch.nn.Linear(in_features=model.fc.in_features, out_features=22)
-
-    model = model.to(device=cfg.device)
 
     logger.info('Creating trainer')
     trainer = Trainer(
@@ -47,7 +63,10 @@ def train(cfg: DictConfig):
     )
     logger.info('Starting training')
     trainer.train(
-        max_epochs=1, experiment_name=cfg.experiment_name, run_name=cfg.model.model_name
+        max_epochs=cfg.max_epochs,
+        experiment_name=cfg.experiment_name,
+        run_name=cfg.model.model_name,
+        cfg=cfg,
     )
 
 
