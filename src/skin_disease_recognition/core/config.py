b8952b3d771b915f234d3f973da06a0e9edc1d1b
jarzeckil <jarzeckil@gmail.com>
2026-02-22 17:33:59 +0100

    refactor: move files to packages

--- src/skin_disease_recognition/core/config.py
+++ src/skin_disease_recognition/core/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 logging.basicConfig(level=logging.INFO)
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent.parent
 
 DATA_DIR = PROJECT_ROOT / 'data'
 RAW_DATA_DIR = DATA_DIR / 'raw'
