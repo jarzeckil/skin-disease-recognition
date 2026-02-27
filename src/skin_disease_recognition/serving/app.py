c67509c4aadbe1b2bca2b4c02490f20f9850f213
jarzeckil <jarzeckil@gmail.com>
2026-02-27 15:52:32 +0100

    feat: add /report endpoint

--- src/skin_disease_recognition/serving/app.py
+++ src/skin_disease_recognition/serving/app.py
@@ -37,6 +37,7 @@ async def lifespan(app: FastAPI):
     metrics_path = os.path.join(model_folder, 'metrics.json')
     data_path = os.path.join(model_folder, 'model_data.json')
     classnames_path = os.path.join(model_folder, 'class_names.txt')
+    classif_report_path = os.path.join(model_folder, 'classification_report.json')
 
     try:
         model: nn.Module = torch.load(
@@ -69,6 +70,13 @@ async def lifespan(app: FastAPI):
         artifacts['classes'] = classes
     except FileNotFoundError as e:
         raise ValueError('Class names not found') from e
+
+    try:
+        with open(classif_report_path) as f:
+            report_data = json.load(f)
+        artifacts['report'] = report_data
+    except FileNotFoundError as e:
+        raise ValueError('Metadata not found') from e
 
     transform = make_transform(artifacts['metadata']['image_size'])
     artifacts['transform'] = transform
@@ -103,7 +111,7 @@ async def predict(file: UploadFile):
 
 @app.get('/info', status_code=status.HTTP_200_OK)
 async def info():
-    metr = artifacts['metrics']
+    metrics = artifacts['metrics']
 
     model_info_response = {
         'model_name': artifacts['metadata']['model_name'],
@@ -111,12 +119,17 @@ async def info():
     }
 
     metrics_response = {
-        'f1': metr['f1'],
-        'accuracy': metr['accuracy'],
-        'recall': metr['recall'],
-        'precision': metr['precision'],
+        'f1': metrics['f1'],
+        'accuracy': metrics['accuracy'],
+        'recall': metrics['recall'],
+        'precision': metrics['precision'],
     }
 
     response = {'model_info': model_info_response, 'metrics': metrics_response}
 
     return response
+
+@app.get('/report', status_code=status.HTTP_200_OK)
+async def report():
+    return artifacts['report']
+
