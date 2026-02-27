05aff8f93b7cb13268e789591e6d0fbaec995316
jarzeckil <jarzeckil@gmail.com>
2026-02-27 13:54:05 +0100

    feat: add downloading more artifacts from mlflow

--- src/skin_disease_recognition/serving/download_model.py
+++ src/skin_disease_recognition/serving/download_model.py
@@ -25,9 +25,9 @@ print(
     f'(Model Version: {latest_version_info.version})'
 )
 
-model = mlflow.pyfunc.load_model(
+model = mlflow.pytorch.load_model(
     f'models:/{MODEL_NAME}/{latest_version_info.version}'
-).get_raw_model()
+)
 model_data = ast.literal_eval(
     client.get_run(run_id).data.to_dictionary()['params']['model']
 )
@@ -35,18 +35,19 @@ metrics = client.get_run(run_id).data.to_dictionary()['metrics']
 
 model_name = model_data['model_name']
 
-os.mkdir(DEST_DIR / model_name)
+path = DEST_DIR / model_name
+os.mkdir(path)
 
-torch.save(model, DEST_DIR / model_name / 'model.pth')
-print(f'Model saved to {DEST_DIR / model_name / "model.pth"}')
+torch.save(model, path / 'model.pth')
+print(f'Model saved to {path / "model.pth"}')
 
-with open(DEST_DIR / model_name / 'model_data.json', 'w') as f:
+model_data['version'] = latest_version_info.version
+with open(path / 'model_data.json', 'w') as f:
     json.dump(model_data, f)
-with open(DEST_DIR / model_name / 'metrics.json', 'w') as f:
+with open(path / 'metrics.json', 'w') as f:
     json.dump(metrics, f)
 
-# TODO download class names from mlflow
-# TODO download classification report
-# TODO pull model version
+mlflow.artifacts.download_artifacts(run_id=run_id, artifact_path='classification_report.txt', dst_path=path)
+mlflow.artifacts.download_artifacts(run_id=run_id, artifact_path='class_names.txt', dst_path=path)
 
 print('Export complete.')
